3f74e874c6247de860b829705d05464593c1fcee
Andy <a.jaxon09@gmail.com>
2019-01-26 19:32:15 -0700

    fixed firebase

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import * as firebase from "firebase";
 import RoomList from "./components/RoomList";
+
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDajPRTcM4OSBvbfTD25VLaj7xqORJCLLU",
-  authDomain: "bloc-chat-d8374.firebaseapp.com",
-  databaseURL: "https://bloc-chat-d8374.firebaseio.com",
-  projectId: "bloc-chat-d8374",
-  storageBucket: "bloc-chat-d8374.appspot.com",
-  messagingSenderId: "430560309092"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyDajPRTcM4OSBvbfTD25VLaj7xqORJCLLU",
+    authDomain: "bloc-chat-d8374.firebaseapp.com",
+    databaseURL: "https://bloc-chat-d8374.firebaseio.com",
+    projectId: "bloc-chat-d8374",
+    storageBucket: "bloc-chat-d8374.appspot.com",
+    messagingSenderId: "430560309092"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
